6f03155751455558695e51e12ab96873f5903c44
Jhanp9 <posadacasaj@gmail.com>
2026-03-17 23:31:46 -0500

    se sube cambio juan

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,6 @@ public class DemoApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("pedro %s!", name);
+      return String.format("juan %s!", name);
     }
 }
