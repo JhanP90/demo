45d15b43dd8cb2c8717191bd2ab60e7fb94b49e9
Jhanp9 <posadacasaj@gmail.com>
2026-03-18 00:11:32 -0500

    se sube el cambio byeee

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,6 @@ public class DemoApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("chaoooo%s!", name);
+      return String.format("byeeee%s!", name);
     }
 }
