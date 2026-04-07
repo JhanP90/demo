c528338c8bdd13f617eea2e47161da6de5da1842
Jhanp9 <posadacasaj@gmail.com>
2026-04-07 16:59:31 -0500

    falla?

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,6 @@ public class DemoApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("listo, funciona%s!", name);
+      return String.format("listo, fallo%s!", name);
     }
 }
