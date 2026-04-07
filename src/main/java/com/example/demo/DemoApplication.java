929c00cbacd955bb60b89dd2f0ccf4575bbd49d4
Jhanp9 <posadacasaj@gmail.com>
2026-04-07 17:02:10 -0500

    sirve ?

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,6 @@ public class DemoApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("listo, fallo%s!", name);
+      return String.format("listo, sirvio%s!", name);
     }
 }
